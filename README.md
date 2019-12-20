c19a84e181cde0f0313c013bb29f9a7af5506472
John Jackson <37978984+johnridesabike@users.noreply.github.com>
2019-12-19 19:30:25 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Learn to Code
+
+## License
+
+## Third party credits
+
+- /src/part2-css/milo.jpg - Jae Park - https://unsplash.com/photos/7GX5aICb5i4
+- /src/part2-css/mittens.jpg - Paul Hanaoka - https://unsplash.com/photos/w2DsS-ZAP4U
+- /src/part2-css/kitty.jpg - Nathalie ANDRE - https://unsplash.com/photos/a8-vQ2Fefig
+
+## Unsplash license
+All photos published on Unsplash can be used for free. You can use them for commercial and noncommercial purposes. You do not need to ask permission from or provide credit to the photographer or Unsplash, although it is appreciated when possible.
+
+More precisely, Unsplash grants you an irrevocable, nonexclusive, worldwide copyright license to download, copy, modify, distribute, perform, and use photos from Unsplash for free, including for commercial purposes, without permission from or attributing the photographer or Unsplash. This license does not include the right to compile photos from Unsplash to replicate a similar or competing service.
---
+++ part3-javascript/script.js
@@ -0,0 +1,34 @@
+console.clear();
+// This is a single-line comment.
+/* This is a
+   multi-line
+   comment */
+var name = "John"; // set a string variable:
+console.log(name.length); // access a property
+// explain the general concepts behind object-oriented programming
+console.log(name.toUpperCase()); // access a method
+var friends = 5; // set a number variable
+friends = friends + 1; // add numbers
+friends++; // shorthand for adding one
+if (friends === 10) { // checking equality
+    console.log("You have ten friends");
+}
+// explain the difference between == and === (short version, if necessary)
+var friendsArray = ["Whitney", "Omar", "Sarah"]; // set an array
+console.log("I have " + friendsArray.length + " friends."); // access array length
+console.log("My best friends is " + friendsArray[0]); // access array index (starting at 1))
+// create a function
+function average(a, b) {
+    var total = a + b;
+    var result = total / 2;
+    return result;
+};
+console.log(average(4, 10));
+
+function friendStats(friends) {
+    console.log("My best friend is" + friends[0]);
+    console.log("I have " + friends.length + " friends.");
+}
+// explain the concept of variable scope, why `friends` can be different in the function
+friendStats(friends);
+friendStats(["John", "Keshan"]);
